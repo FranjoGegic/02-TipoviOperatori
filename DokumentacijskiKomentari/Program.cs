de07cea572a81ff7a35527a52a5d3e215f03ea12
Franjo Gegic <frgegic@vsite.hr>
2016-10-13 21:09:55 +0200

    Dokumentacijski komentari

--- DokumentacijskiKomentari/Program.cs
+++ DokumentacijskiKomentari/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace CSharp.TipoviOperatori
 {
+    /// <summary>
+    ///     Ovo je samo moja vjezba pisanja komentara.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -27,7 +30,12 @@ namespace CSharp.TipoviOperatori
             return broj * broj;
         }
 
-
+        /// <summary>
+        ///     Izracunava ukupnu duljinu dvije rijeci.
+        /// </summary>
+        /// <param name="prva">Prva rijec.</param>
+        /// <param name="druga">Druga rijec.</param>
+        /// <returns>Zbroj duljina.</returns>
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return (prva + druga).Length;
